ed00c7705dd79b5d3908421e06d2c8db054f1c38
vernonzheng <kevonzheng@gmail.com>
2015-07-20 18:50:36 +0800

    do refactoring

--- src/hades_web/log.clj
+++ src/hades_web/log.clj
@@ -11,18 +11,32 @@
 (defn get-last-log []
   @mem-log)
 
+(defn generate-log
+  [msg]
+  (let [user (get-user)
+    prev-msg (str (now-string) ",user:" user)
+    log-msg  (str prev-msg ",detail:" msg)]
+  log-msg))
+
 (defn log->memory
   [msg]
-  (println msg)
   (if (>= (count @mem-log) max-mem-log)
     (dosync (ref-set mem-log (drop-last drop-size @mem-log))))
-  (dosync (alter mem-log conj msg)))
+  (dosync (alter mem-log conj (str msg "</br>")))
+  msg)
+
+(defn log->file
+  [msg]
+  (spit (str "operation-" (date-string) ".log") (str msg "\n") :append true)
+  msg)
+
+(defn log->mutil
+  [msg]
+  (-> (generate-log msg)
+    (log->memory)
+    (log->file)))
 
 (defn oper-log
   "Operation log when invoke CURD .etc"
   [msg]
-  (let [user (get-user)
-        prev-msg (str (now-string) ",user:" user)
-        log-msg  (str prev-msg ",detail:" msg)]
-    (log->memory (str log-msg "</br>"))
-    (spit (str "operation-" (date-string) ".log") (str log-msg "\n") :append true)))
+  (log->mutil msg))
